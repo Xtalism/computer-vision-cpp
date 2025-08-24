4dba323b6345c3dff8bab61af4fe074fb0ef590f
Xtalism <manuel_lego1234@hotmail.com>
2025-08-24 02:13:01 -0600

    CMake, OpenCV C++ test program

---
+++ build/CMakeFiles/computer_vision_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computer_vision_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/computer_vision_cpp.dir/src/main.cpp.o.d"
+  "computer_vision_cpp"
+  "computer_vision_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computer_vision_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
